cdfb7fc19a60ea119ad8c80d77ae87bb1aedebc8
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 11:42:48 +0100

    Update create_timestamps_table.php

--- src/Migrations/create_timestamps_table.php
+++ src/Migrations/create_timestamps_table.php
@@ -2,7 +2,35 @@
 
 namespace Lamine\License\Migrations;
 
-class create_timestamps_table
-{
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
+class create_timestamps_table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timestamps', function (Blueprint $table) {
+            $table->id();
+            $table->string('timestamp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('timestamps', function (Blueprint $table) {
+            //
+        });
+    }
 }
