2d46426883ce0976cb4736e3acc7b0457354511e
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 11:25:50 +0100

    Update ApiWare.php

--- src/Middleware/ApiWare.php
+++ src/Middleware/ApiWare.php
@@ -2,7 +2,28 @@
 
 namespace Lamine\License\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use Lamine\License\Models\ProductKey;
+
 class ApiWare
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $license = ProductKey::first();
 
+        if (!$license || $license->isExpired()) {
+            return response()->json([
+                'message' => 'Product Not Active or License Expired'
+            ], 401);
+        }
+        return $next($request);
+    }
 }
