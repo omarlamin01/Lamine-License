fc49a8762184778f0c1f7e15e5ebf8570863a729
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 11:40:17 +0100

    Update create_license_table.php

--- src/Migrations/create_license_table.php
+++ src/Migrations/create_license_table.php
@@ -2,7 +2,39 @@
 
 namespace Lamine\License\Migrations;
 
-class create_license_table
-{
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
+class create_license_table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('license', function (Blueprint $table) {
+            $table->id();
+            $table->string('license_key');
+            $table->string('license_type');
+            $table->string('mac')->nullable();
+            $table->string('cpu')->nullable();
+            $table->string('motherboard')->nullable();
+            $table->date('expires_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('license');
+    }
 }
