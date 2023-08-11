b50e9836297a6e95d94092cb3794cd9de48e1f3b
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 11:59:05 +0100

    Update TimeChecker.php

--- src/Tools/TimeChecker.php
+++ src/Tools/TimeChecker.php
@@ -2,7 +2,50 @@
 
 namespace Lamine\License\Tools;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Lamine\License\Models\ProductKey;
+
 class TimeChecker
 {
+    static function checkTime() {
+        $previous_time = DB::table('timestamps')->orderBy('id', 'desc')->first();
+        $current_time = time();
 
+        $license = ProductKey::first();
+
+        if ($previous_time) {
+            $previous_time = $previous_time->timestamp;
+            if ($current_time < $previous_time) {
+                Log::info('Time changed.');
+                Log::info('previous time: ' . $previous_time);
+                Log::info('current time: ' . $current_time);
+
+                if ($license) {
+                    Log::info('Updating license...');
+                    Log::debug('expires_at: ' . $license->expires_at);
+                    Log::debug('new expiry date: ' . date('Y-m-d H:i:s', $current_time + (strtotime($license->expires_at) - $previous_time)));
+                    $license->update([
+                        'expires_at' => date('Y-m-d H:i:s', $current_time + (strtotime($license->expires_at) - $previous_time))
+                    ]);
+                }
+            } else {
+                Log::info('Time check passed.');
+                Log::info('previous time: ' . $previous_time);
+                Log::info('current time: ' . $current_time);
+            }
+        }
+
+        DB::table('timestamps')->insert([
+            'timestamp' => $current_time
+        ]);
+    }
+
+    static function truncateTimestamps() {
+        DB::table('timestamps')->truncate();
+        DB::table('timestamps')->insert([
+            'timestamp' => time()
+        ]);
+        Log::info('Truncated timestamps table.');
+    }
 }
