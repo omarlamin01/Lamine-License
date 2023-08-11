8a5928fd0e71f74d5fda515c389f3f8e9306670e
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 11:29:57 +0100

    Update WebWare.php

--- src/Middleware/WebWare.php
+++ src/Middleware/WebWare.php
@@ -2,7 +2,23 @@
 
 namespace Lamine\License\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
 class WebWare
 {
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!session('license_is_valid')) {
+            return redirect()->route('license');
+        }
+        return $next($request);
+    }
 }
