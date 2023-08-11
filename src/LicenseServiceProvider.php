12b9fe579cb36c9fc77835ad1c424dec4fd690c5
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-08-11 10:36:22 +0100

    some changes

---
+++ src/LicenseServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lamine\License;
+
+use Illuminate\Support\ServiceProvider;
+
+class LicenseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+
+    }
+}
