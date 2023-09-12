8636d73700936b0038421c9aa3c80b50482cb0c9
omarlamin01 <omarlamin272@gmail.com>
2023-09-12 16:07:09 +0100

    Update LicenseServiceProvider.php

--- src/LicenseServiceProvider.php
+++ src/LicenseServiceProvider.php
@@ -11,17 +11,23 @@ class LicenseServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Load migrations
         $this->loadMigrationsFrom(__DIR__.'/Migrations');
 
+        // Load views
         $this->loadViewsFrom(__DIR__.'/Views', 'Lamine/License');
 
-        $this->publishes([
-            __DIR__.'/Config/license.php' => config_path('license.php'),
-        ], 'config');
+        // Load Routes
+        $this->loadRoutesFrom(__DIR__ . '/Routes/index.php');
 
         $this->publishes([
-            __DIR__.'/Views' => resource_path('views/vendor/Lamine/License'),
-        ], 'views');
+            __DIR__.'/Migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        // publish routes
+        $this->publishes([
+            __DIR__.'/Routes/index.php' => base_path('routes/license.php'),
+        ], 'routes');
     }
 
     /**
