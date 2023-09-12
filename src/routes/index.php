9a737319bb41cfef99b2d908a92a02274b8e8cb9
omarlamin01 <omarlamin272@gmail.com>
2023-09-12 18:38:12 +0100

    Update index.php

--- src/routes/index.php
+++ src/routes/index.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Lamine\License\Controllers\Index;
 
 Route::get('/activate-product', function () {
-    return view('LicenseExpired');
+    return view('License::LicenseExpired');
 })->name('license');
 
 Route::post('/activate-product', [Index::class, 'webStore'])->name('license.store');
